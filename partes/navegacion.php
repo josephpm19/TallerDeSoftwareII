883839108117a1a6c1c59e33f586e627e5562060
Jose Luis Paquiyauri <joshephpm@gmail.com>
2018-10-27 17:29:01 -0500

    catalogo con titulo

--- partes/navegacion.php
+++ partes/navegacion.php
@@ -1,7 +1,7 @@
 <div id="menu">
     <ul>
         <li>  <a href="index.php">Home </a> </li>
-        <li> <a href="#"> Catalogo</a> </li>
+        <li> <a href="catalogo.php"> Catalogo</a> </li>
         <li> <a href="#"> Sobre Nosotros</a> </li>
         <li> <a href="#"> Contactenos</a> </li>
         <li> <a href="#"> Nuestro Equipo</a> </li>
