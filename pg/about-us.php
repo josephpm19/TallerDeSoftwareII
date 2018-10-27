5c225628e8300c04ef702a8b8ec2f84876bf8e9d
Jose Luis Paquiyauri <joshephpm@gmail.com>
2018-10-27 17:42:39 -0500

    paginas

---
+++ pg/about-us.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Iniciar Sesion | Pisquerito</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" media="screen" href="css/login.css" />
+	<link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
+
+	<link rel="shortcut icon" type="image/x-icon" href="img\ico.png" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+	<link href='https://fonts.googleapis.com/css?family=Nunito:400,300' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<?php include 'partes/header.php' ?>
+	<?php include 'partes/navegacion.php' ?>
+    <h1>Sobre Nosotros</h1>
+
+    
+    
+
+
+
+	<?php include 'partes/footer.php' ?>
+
+</body>
+</html>
