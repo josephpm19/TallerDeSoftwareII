44d40c12251c52e69999fa81fe16a53ecd88ff94
Jose Luis Paquiyauri <joshephpm@gmail.com>
2018-10-27 19:06:54 -0500

    estilos de formulario

--- pg/contacto.php
+++ pg/contacto.php
@@ -5,20 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Iniciar Sesion | Pisquerito</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" media="screen" href="css/login.css" />
-	<link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
+	<link rel="stylesheet" type="text/css" media="screen" href="../css/login.css" />
+	<link rel="stylesheet" type="text/css" media="screen" href="../css/main.css" />
 
 	<link rel="shortcut icon" type="image/x-icon" href="img\ico.png" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
 	<link href='https://fonts.googleapis.com/css?family=Nunito:400,300' rel='stylesheet' type='text/css'>
 </head>
 <body>
-	<?php include 'partes/header.php' ?>
-	<?php include 'partes/navegacion.php' ?>
-    <main>
-        <h1>Contáctenos</h1>
+	<?php include '../partes/header.php' ?>
+	<?php include '../partes/navegacion.php' ?>
+        
+<div id="contacto">
+    <h1>Contáctenos</h1>
     <form action="contacto_procesar.php" method="post">
-        <div>
+        <div class="login-form">
             <label for="nombre">Nombre:</label>
             <input type="text" name="nombre"
 
@@ -27,7 +28,7 @@
             <?php } ?>
             >
         </div>
-        <div>
+        <div class="login-form">
             <label for="correo">Correo: </label>
             <input type="email" name="correo"
             <?php  if (isset($_SESSION["correo"])){?>
@@ -35,33 +36,33 @@
             <?php } ?>
             >
         </div>
-		<div>
-            <label for="mensaje">:Telefono</label>
-            <textarea name="mensaje" rows="4" cols="50">
-            </textarea>
+		<div class="login-form">
+            <label for="telefono">Telefono:</label>
+            <input type="text" name="telefono">
+
         </div>
-        <div>
+        <div class="login-form">
             <label for="pedido">Numero de pedido: </label>
             <input type="text" name="pedido">
         </div>
-        <div>
+        <div class="login-form">
             <label for="comentario">Comentario: </label>
             <textarea name="comentario" rows="4" cols="50">
             </textarea>
         </div>
-        <div>
-            <button input="submit">Enviar</button>
+        <div >
+            <button class="btn-login" input="submit">Enviar</button>
         </div>
     </form>
-    </main>
-
+</div>
+    
 
     
     
 
 
 
-	<?php include 'partes/footer.php' ?>
+	<?php include '../partes/footer.php' ?>
 
 </body>
 </html>
--- pg/politicas-de-privacidad.php
+++ pg/politicas-de-privacidad.php
@@ -20,7 +20,7 @@
     
     <h1>Politicas de Privacidad</h1>
 
-    <div id="´pol">
+    <div>
         <h2>The standard Lorem Ipsum passage, used since the 1500s</h2>
         <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
 
