a0938f45d3f91bdae8693c659c0e0f3a37526e00
luis alzamora <luisalzamora.2803@gmail.com>
2018-10-30 08:40:25 -0700

    modificacion para terminado

--- contacto.php
+++ contacto.php
@@ -5,16 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Iniciar Sesion | Pisquerito</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" media="screen" href="../css/login.css" />
-	<link rel="stylesheet" type="text/css" media="screen" href="../css/main.css" />
+	<link rel="stylesheet" type="text/css" media="screen" href="css/login.css" />
+	<link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
 
 	<link rel="shortcut icon" type="image/x-icon" href="img\ico.png" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
 	<link href='https://fonts.googleapis.com/css?family=Nunito:400,300' rel='stylesheet' type='text/css'>
 </head>
 <body>
-	<?php include '../partes/header.php' ?>
-	<?php include '../partes/navegacion.php' ?>
+	<?php include 'partes/header.php' ?>
+	<?php include 'partes/navegacion.php' ?>
         
 <div id="contacto">
     <h1>Contáctenos</h1>
@@ -62,7 +62,7 @@
 
 
 
-	<?php include '../partes/footer.php' ?>
+	<?php include 'partes/footer.php' ?>
 
 </body>
 </html>
