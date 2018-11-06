ae9d0e914bec44d1420cebd0e484f77f30fd189d
Jose Luis Paquiyauri <joshephpm@gmail.com>
2018-11-06 10:49:20 -0500

    panel de administracion

---
+++ admin/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Panel de Administracion | Pisquerito</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
+    <link href='https://fonts.googleapis.com/css?family=Nunito:400,300' rel='stylesheet' type='text/css'>
+</head>
+<body>
+    <div class="cabecera">
+    </div>
+    <aside class="sidebar-menu">
+        <div class="logo"> <img src="img/logo.png" alt=""></div>
+        <div class="perfil">
+          <div class="foto-perfil"><img src="img/6.jpg" alt="Foto Perfil"></div>  
+          <div class="nombre-perfil"> Bienvenido <span>José Luis P</span></div>
+        </div>
+        <nav class="navegacion">
+            <ul>
+                <li></li>
+                <li>
+                    <a href="index.php"><span>Home</span> </a>
+                </li>
+                <li>
+                <a href="index.php"><span>Home</span> </a>
+                </li>
+                <li>
+                <a href="index.php"><span>Home</span> </a>
+                </li>
+                <li>
+                <a href="index.php"><span>Home</span> </a>
+                </li>
+                <li>
+                <a href="index.php"><span>Home</span> </a>
+                </li>
+                <li>
+                <a href="index.php"><span>Home</span> </a>
+                </li>
+            </ul>
+        </nav>
+    </aside>
+</body>
+</html>
+
+
+
+
