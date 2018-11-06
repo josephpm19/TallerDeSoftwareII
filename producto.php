46ac91091d5c8e37b9d048072b5e3d7e562e9420
Jose Luis Paquiyauri <joshephpm@gmail.com>
2018-11-06 10:48:02 -0500

    pagina de producto

---
+++ producto.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inicio | Pisquerito</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/login.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/producto.css" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"></head>
+    <link href='https://fonts.googleapis.com/css?family=Nunito:400,300' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+    <?php include 'partes/header.php' ?>
+    <?php include 'partes/navegacion.php' ?>
+    <div class="img-title">
+        <div class="imagen-producto">
+            <img src="https://http2.mlstatic.com/3-pisco-peruano-santiago-queirolo-quebranta-italia-legitimo-D_NQ_NP_748808-MLB26890124435_022018-F.jpg" alt="">
+         </div>
+        <div class="bloque-datos">
+            <div class="titulo">
+                TITULO DEL PRODUCTO
+            </div>
+             <div class="precio">
+            </div>
+            <div class="btn-cart">
+            </div>
+    </div>
+   
+    <div class="description">
+    </div>
+
+    </div>
+    
+    <?php include 'partes/footer.php'?>
+</body>
+</html>
